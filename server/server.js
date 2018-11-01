23abcf27ab6b71ec9135f663ea78c15f75c2bf4b
Oscar Montes <o.montescamberos@ugto.mx>
2018-11-01 14:15:53 -0600

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 let app = express();
@@ -13,27 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {//registrar eventlistener
     console.log('Client connected.');
 
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat.',
-        createAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app.'));
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'An user joined to the chat',
-        createAt: new Date().getTime()
-    });    
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'An user joined to the chat.'));    
 
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
 
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
 
         // socket.broadcast.emit('newMessage', { //que todos vean el mensaje menos el usuario que lo manda
         //     from: message.from,
